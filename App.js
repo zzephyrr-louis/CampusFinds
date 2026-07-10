3d49317ba2a02d86c3617661470542038be39e7c
LAB27_USER ON WS22 <L27X16W22@LabHCI.net>
2026-07-10 15:32:20 +0800

    feat: Set up initial project structure and styling

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import DashboardPage from './pages/DashboardPage';
+import './App.css';
 
 function App() {
   return (
